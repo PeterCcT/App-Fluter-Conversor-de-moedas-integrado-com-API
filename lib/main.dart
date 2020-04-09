94df1b1ee30e3cfaded107dc103134b54e816e28
PeterCcT <pedrorodriguesfereira@gmail.com>
2020-04-09 17:14:41 -0300

    Layout inicial

--- lib/main.dart
+++ lib/main.dart
@@ -1,111 +1,135 @@
 import 'package:flutter/material.dart';
+import 'package:http/http.dart' as http;
+import 'dart:async';
+import 'dart:convert';
 
-void main() => runApp(MyApp());
+const String request =
+    "https://api.hgbrasil.com/finance?format=json-cors&key=5d207b77";
 
-class MyApp extends StatelessWidget {
-  // This widget is the root of your application.
-  @override
-  Widget build(BuildContext context) {
-    return MaterialApp(
-      title: 'Flutter Demo',
-      theme: ThemeData(
-        // This is the theme of your application.
-        //
-        // Try running your application with "flutter run". You'll see the
-        // application has a blue toolbar. Then, without quitting the app, try
-        // changing the primarySwatch below to Colors.green and then invoke
-        // "hot reload" (press "r" in the console where you ran "flutter run",
-        // or simply save your changes to "hot reload" in a Flutter IDE).
-        // Notice that the counter didn't reset back to zero; the application
-        // is not restarted.
-        primarySwatch: Colors.blue,
-      ),
-      home: MyHomePage(title: 'Flutter Demo Home Page'),
-    );
-  }
+void main() async {
+  runApp(
+    MaterialApp(
+      title: "Conversor de moedas",
+      home: Home(),
+    ),
+  );
 }
 
-class MyHomePage extends StatefulWidget {
-  MyHomePage({Key key, this.title}) : super(key: key);
-
-  // This widget is the home page of your application. It is stateful, meaning
-  // that it has a State object (defined below) that contains fields that affect
-  // how it looks.
-
-  // This class is the configuration for the state. It holds the values (in this
-  // case the title) provided by the parent (in this case the App widget) and
-  // used by the build method of the State. Fields in a Widget subclass are
-  // always marked "final".
-
-  final String title;
-
+class Home extends StatefulWidget {
   @override
-  _MyHomePageState createState() => _MyHomePageState();
+  _HomeState createState() => _HomeState();
 }
 
-class _MyHomePageState extends State<MyHomePage> {
-  int _counter = 0;
-
-  void _incrementCounter() {
-    setState(() {
-      // This call to setState tells the Flutter framework that something has
-      // changed in this State, which causes it to rerun the build method below
-      // so that the display can reflect the updated values. If we changed
-      // _counter without calling setState(), then the build method would not be
-      // called again, and so nothing would appear to happen.
-      _counter++;
-    });
-  }
-
+class _HomeState extends State<Home> {
   @override
   Widget build(BuildContext context) {
-    // This method is rerun every time setState is called, for instance as done
-    // by the _incrementCounter method above.
-    //
-    // The Flutter framework has been optimized to make rerunning build methods
-    // fast, so that you can just rebuild anything that needs updating rather
-    // than having to individually change instances of widgets.
     return Scaffold(
+      backgroundColor: Colors.indigo[500],
       appBar: AppBar(
-        // Here we take the value from the MyHomePage object that was created by
-        // the App.build method, and use it to set our appbar title.
-        title: Text(widget.title),
+        title: Text("Conversor de moedas"),
+        centerTitle: true,
+        backgroundColor: Color.fromRGBO(18, 21, 76, 1),
       ),
-      body: Center(
-        // Center is a layout widget. It takes a single child and positions it
-        // in the middle of the parent.
-        child: Column(
-          // Column is also a layout widget. It takes a list of children and
-          // arranges them vertically. By default, it sizes itself to fit its
-          // children horizontally, and tries to be as tall as its parent.
-          //
-          // Invoke "debug painting" (press "p" in the console, choose the
-          // "Toggle Debug Paint" action from the Flutter Inspector in Android
-          // Studio, or the "Toggle Debug Paint" command in Visual Studio Code)
-          // to see the wireframe for each widget.
-          //
-          // Column has various properties to control how it sizes itself and
-          // how it positions its children. Here we use mainAxisAlignment to
-          // center the children vertically; the main axis here is the vertical
-          // axis because Columns are vertical (the cross axis would be
-          // horizontal).
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            Text(
-              'You have pushed the button this many times:',
-            ),
-            Text(
-              '$_counter',
-              style: Theme.of(context).textTheme.display1,
-            ),
-          ],
+      body: SingleChildScrollView(
+        child: Form(
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.stretch,
+            children: <Widget>[
+              Icon(
+                Icons.monetization_on,
+                color: Color.fromRGBO(50, 150, 212, 1),
+                size: 120,
+              ),
+              Padding(
+                padding: EdgeInsets.symmetric(vertical: 15, horizontal: 15),
+                child: TextFormField(
+                  keyboardType: TextInputType.number,
+                  style: TextStyle(
+                      color: Colors.white, fontSize: 15, letterSpacing: 2),
+                  decoration: InputDecoration(
+                    border: InputBorder.none,
+                    focusedBorder: OutlineInputBorder(
+                        borderRadius: BorderRadius.circular(20),
+                        borderSide: BorderSide(color: Colors.white)),
+                    prefixText: "RS",
+                    prefixStyle: TextStyle(
+                      color: Color.fromRGBO(250, 250, 250, 0.7),
+                      fontSize: 15,
+                    ),
+                    hintText: "0.00",
+                    hintStyle: TextStyle(
+                      color: Color.fromRGBO(250, 250, 250, 0.4),
+                      fontSize: 15,
+                    ),
+                    labelText: "BRL",
+                    labelStyle: TextStyle(color: Colors.white, fontSize: 20),
+                  ),
+                ),
+              ),
+              Padding(
+                padding: EdgeInsets.symmetric(vertical: 15, horizontal: 15),
+                child: TextFormField(
+                  keyboardType: TextInputType.number,
+                  style: TextStyle(
+                      color: Colors.white, fontSize: 15, letterSpacing: 2),
+                  decoration: InputDecoration(
+                    border: InputBorder.none,
+                    focusedBorder: OutlineInputBorder(
+                        borderRadius: BorderRadius.circular(20),
+                        borderSide: BorderSide(
+                          color: Colors.white,
+                        )),
+                    prefixText: "US",
+                    prefixStyle: TextStyle(
+                      color: Color.fromRGBO(250, 250, 250, 0.7),
+                      fontSize: 15,
+                    ),
+                    hintText: "0.00",
+                    hintStyle: TextStyle(
+                      color: Color.fromRGBO(250, 250, 250, 0.4),
+                      fontSize: 15,
+                    ),
+                    labelText: "USD",
+                    labelStyle: TextStyle(color: Colors.white, fontSize: 20),
+                  ),
+                ),
+              ),
+              Padding(
+                padding: EdgeInsets.symmetric(vertical: 15, horizontal: 15),
+                child: TextFormField(
+                  keyboardType: TextInputType.number,
+                  style: TextStyle(
+                      color: Colors.white, fontSize: 15, letterSpacing: 2),
+                  decoration: InputDecoration(
+                    border: InputBorder.none,
+                    focusedBorder: OutlineInputBorder(
+                      borderRadius: BorderRadius.circular(20),
+                      borderSide: BorderSide(color: Colors.white),
+                    ),
+                    prefixText: "€",
+                    prefixStyle: TextStyle(
+                      color: Color.fromRGBO(250, 250, 250, 0.7),
+                      fontSize: 15,
+                    ),
+                    hintText: "0.00",
+                    hintStyle: TextStyle(
+                      color: Color.fromRGBO(250, 250, 250, 0.4),
+                      fontSize: 15,
+                    ),
+                    labelText: "EUR",
+                    labelStyle: TextStyle(color: Colors.white, fontSize: 20),
+                  ),
+                ),
+              ),
+            ],
+          ),
         ),
       ),
-      floatingActionButton: FloatingActionButton(
-        onPressed: _incrementCounter,
-        tooltip: 'Increment',
-        child: Icon(Icons.add),
-      ), // This trailing comma makes auto-formatting nicer for build methods.
     );
+  }
+
+  Future<Map> getData() async {
+    http.Response resposta = await http.get(request);
+    return json.decode(resposta.body);
   }
 }
