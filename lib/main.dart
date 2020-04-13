2d123e6506de2d29a4e51ba1bed0feb3a3529b8e
PeterCcT <pedrorodriguesfereira@gmail.com>
2020-04-13 13:59:20 -0300

    Melhoria no código

--- lib/main.dart
+++ lib/main.dart
@@ -24,11 +24,11 @@ class _HomeState extends State<Home> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      backgroundColor: Colors.indigo[500],
+      backgroundColor: Colors.amber,
       appBar: AppBar(
         title: Text("Conversor de moedas"),
         centerTitle: true,
-        backgroundColor: Color.fromRGBO(18, 21, 76, 1),
+        backgroundColor: Colors.black,
       ),
       body: SingleChildScrollView(
         child: Form(
@@ -37,89 +37,20 @@ class _HomeState extends State<Home> {
             children: <Widget>[
               Icon(
                 Icons.monetization_on,
-                color: Color.fromRGBO(50, 150, 212, 1),
+                color: Colors.amber,
                 size: 120,
               ),
               Padding(
                 padding: EdgeInsets.symmetric(vertical: 15, horizontal: 15),
-                child: TextFormField(
-                  keyboardType: TextInputType.number,
-                  style: TextStyle(
-                      color: Colors.white, fontSize: 15, letterSpacing: 2),
-                  decoration: InputDecoration(
-                    border: InputBorder.none,
-                    focusedBorder: OutlineInputBorder(
-                        borderRadius: BorderRadius.circular(20),
-                        borderSide: BorderSide(color: Colors.white)),
-                    prefixText: "R\$",
-                    prefixStyle: TextStyle(
-                      color: Color.fromRGBO(250, 250, 250, 0.7),
-                      fontSize: 15,
-                    ),
-                    hintText: "0.00",
-                    hintStyle: TextStyle(
-                      color: Color.fromRGBO(250, 250, 250, 0.4),
-                      fontSize: 15,
-                    ),
-                    labelText: "BRL",
-                    labelStyle: TextStyle(color: Colors.white, fontSize: 20),
-                  ),
-                ),
+                child: buildTextField("BRL", "R\$"),
               ),
               Padding(
                 padding: EdgeInsets.symmetric(vertical: 15, horizontal: 15),
-                child: TextFormField(
-                  keyboardType: TextInputType.number,
-                  style: TextStyle(
-                      color: Colors.white, fontSize: 15, letterSpacing: 2),
-                  decoration: InputDecoration(
-                    border: InputBorder.none,
-                    focusedBorder: OutlineInputBorder(
-                        borderRadius: BorderRadius.circular(20),
-                        borderSide: BorderSide(
-                          color: Colors.white,
-                        )),
-                    prefixText: "U\$",
-                    prefixStyle: TextStyle(
-                      color: Color.fromRGBO(250, 250, 250, 0.7),
-                      fontSize: 15,
-                    ),
-                    hintText: "0.00",
-                    hintStyle: TextStyle(
-                      color: Color.fromRGBO(250, 250, 250, 0.4),
-                      fontSize: 15,
-                    ),
-                    labelText: "USD",
-                    labelStyle: TextStyle(color: Colors.white, fontSize: 20),
-                  ),
-                ),
+                child: buildTextField("USD", "U\$"),
               ),
               Padding(
                 padding: EdgeInsets.symmetric(vertical: 15, horizontal: 15),
-                child: TextFormField(
-                  keyboardType: TextInputType.number,
-                  style: TextStyle(
-                      color: Colors.white, fontSize: 15, letterSpacing: 2),
-                  decoration: InputDecoration(
-                    border: InputBorder.none,
-                    focusedBorder: OutlineInputBorder(
-                      borderRadius: BorderRadius.circular(20),
-                      borderSide: BorderSide(color: Colors.white),
-                    ),
-                    prefixText: "€",
-                    prefixStyle: TextStyle(
-                      color: Color.fromRGBO(250, 250, 250, 0.7),
-                      fontSize: 15,
-                    ),
-                    hintText: "0.00",
-                    hintStyle: TextStyle(
-                      color: Color.fromRGBO(250, 250, 250, 0.4),
-                      fontSize: 15,
-                    ),
-                    labelText: "EUR",
-                    labelStyle: TextStyle(color: Colors.white, fontSize: 20),
-                  ),
-                ),
+                child: buildTextField("EUR", "€"),
               ),
             ],
           ),
@@ -131,5 +62,32 @@ class _HomeState extends State<Home> {
   Future<Map> getData() async {
     http.Response resposta = await http.get(request);
     return json.decode(resposta.body);
+  }
+
+  Widget buildTextField(String label, String prefixo) {
+    return TextFormField(
+      keyboardType: TextInputType.number,
+      style: TextStyle(color: Colors.white, fontSize: 15, letterSpacing: 2),
+      decoration: InputDecoration(
+        border: InputBorder.none,
+        focusedBorder: OutlineInputBorder(
+            borderRadius: BorderRadius.circular(20),
+            borderSide: BorderSide(
+              color: Colors.white,
+            )),
+        prefixText: prefixo,
+        prefixStyle: TextStyle(
+          color: Color.fromRGBO(250, 250, 250, 0.7),
+          fontSize: 15,
+        ),
+        hintText: "0.00",
+        hintStyle: TextStyle(
+          color: Color.fromRGBO(250, 250, 250, 0.4),
+          fontSize: 15,
+        ),
+        labelText: label,
+        labelStyle: TextStyle(color: Colors.white, fontSize: 20),
+      ),
+    );
   }
 }
